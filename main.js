28b281e3756c964b5fb6ca6f6d586bf6422e44bc
RubenCodeTech <pzgz.ruben@gmail.com>
2022-11-16 23:19:47 +0100

    form validation, using regex with js, add button from saquitos section now add a product in local storage, in the shop page that element appears, some beta stetic on that cartshop just for the moment. Navbar and first section, have a css and html remake, now is more simple, some classes now are change

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 // FAQ START
-arrow.onclick = function(){
-    if(textFaq.hidden == true){
+arrow.onclick = function () {
+    if (textFaq.hidden == true) {
         textFaq.hidden = false
         arrow.style.transform = 'rotate(0deg)';
-    }else{
-        textFaq.hidden=true
+    } else {
+        textFaq.hidden = true
         arrow.style.transform = 'rotate(180deg)';
     }
 }
-arrowSecond.onclick = function (){
-    if(text_Faq_Second.hidden == true){
+arrowSecond.onclick = function () {
+    if (text_Faq_Second.hidden == true) {
         text_Faq_Second.hidden = false
         arrowSecond.style.transform = 'rotate(0deg)';
-    } else{
+    } else {
         text_Faq_Second.hidden = true
         arrowSecond.style.transform = 'rotate(180deg)';
     }
 }
-arrowThird.onclick =  function (){
-    if(text_Faq_Third.hidden == true){
+arrowThird.onclick = function () {
+    if (text_Faq_Third.hidden == true) {
         text_Faq_Third.hidden = false
         arrowThird.style.transform = 'rotate(0deg)';
-    }else{
+    } else {
         text_Faq_Third.hidden = true;
-        
+
         arrowThird.style.transform = 'rotate(180deg)';
     }
 }
@@ -34,25 +34,32 @@ arrowThird.onclick =  function (){
 let coffeePrize = document.getElementsByClassName('prizeCoffee')[0]
 // let coffeeName = document.getElementsByClassName('coffeeNames')[0]
 let addButtonShop = document.getElementsByClassName('addbutton')
-for(i=0; i< addButtonShop.length; i++){
-   addButtonShop[i].onclick = function(){
-       
+for (i = 0; i < addButtonShop.length; i++) {
+    addButtonShop[i].onclick = function () {
+
         let imgShopItems = this.parentElement.querySelector('img');
         let prizeShopItems = this.parentElement.getElementsByClassName('prizeCoffee')[0];
         let tittleShopItems = this.parentElement.getElementsByClassName('coffeNames')[0];
-        let cestaCompra = document.getElementById('cestaCompra')
-        localStorage.setItem("prize",prizeShopItems.textContent);
-        localStorage.setItem("name",tittleShopItems.textContent);
-        localStorage.setItem("img",imgShopItems.src);
+
+        let thePrize = prizeShopItems.textContent;
+        let theTittle = tittleShopItems.textContent;
+        let theImg = imgShopItems.src;
+
+        let dataCartShop = {
+            img: theImg,
+            tittle: theTittle,
+            name: thePrize
+        }
+        console.log(dataCartShop)
+        localStorage.setItem('dataCartShop', JSON.stringify(dataCartShop));
         // cestaCompra.textContent = localStorage.getItem("prize");
         // cestaCompra.textContent = localStorage.getItem("name");
         // cestaCompra.textContent = localStorage.getItem("img");
-        
-        
+
         // cestaCompra.innerText = localStorage.getItem("name");
         // cestaCompra.innerText = localStorage.getItem("img");
         console.log(tittleShopItems, imgShopItems, prizeShopItems);
-    
+
     }
     // addButtonShop[i].addEventListener('click', add)
 }
@@ -88,4 +95,111 @@ for(i=0; i< addButtonShop.length; i++){
 //     let currentValue = e.target.value;
 //     console.log(pattern.test(currentValue));
 // });
+const form = document.getElementById('form');
+const inputsForm = document.querySelectorAll('#form input');
+console.log(inputsForm)
+const regExFormObject = {
+    usuario: /^[a-zA-Z\s]{4,24}$/,
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-z]{1,3}$/,
+    telf: /^\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{3})$/,
+    opinion: /^[a-zA-Z0-9]{1,140}$/,
+}
+// document.querySelector()
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     if (inputsForm.value.length === "") {
+//         alert("mal");
+//       }
+// });
+// function validateForm() {
+//     if (inputsForm == "") {
+//       alert("mal");
+//     }
+//   }
+const verifyEverySingleCharacter = (e) => {
+    switch (e.target.name) {
+        case "username":
+            if (regExFormObject.usuario.test(e.target.value) == true) {
+                document.querySelector('#username').classList.remove('formInvalid');
+                document.querySelector('#username').classList.add('formAcepted');
+                document.querySelector('.formName .formTextError').classList.remove('formTextErrorActive')
+                // verify.usuario = true;
+            } else {
+                document.querySelector('#username').classList.add('formInvalid');
+                document.querySelector('#username').classList.remove('formAcepted');
+                document.querySelector('.formName .formTextError').classList.add('formTextErrorActive')
+                // verify[usuario] = false ;
+            }
+            break;
+        case "email":
+            if (regExFormObject.email.test(e.target.value) == true) {
+                document.querySelector('#email').classList.remove('formInvalid');
+                document.querySelector('#email').classList.add('formAcepted');
+                document.querySelector('.formEmail .formTextError').classList.remove('formTextErrorActive')
+                // verify['email'] = true;
+            } else {
+                document.querySelector('#email').classList.add('formInvalid');
+                document.querySelector('#email').classList.remove('formAcepted');
+                document.querySelector('.formEmail .formTextError').classList.add('formTextErrorActive')
+                // verify['email'] = false;
+            }
+            break;
+        case "tlf":
+            if (regExFormObject.telf.test(e.target.value) == true) {
+                document.querySelector('#tlf').classList.remove('formInvalid');
+                document.querySelector('#tlf').classList.add('formAcepted');
+                document.querySelector('.formerrortelf .formTextError').classList.remove('formTextErrorActive')
+                // verify['telf'] = true;
+            } else {
+                document.querySelector('#tlf').classList.add('formInvalid');
+                document.querySelector('#tlf').classList.remove('formAcepted');
+                document.querySelector('.formTelf .formTextError').classList.add('formTextErrorActive')
+                // verify['telf'] = false;
+            }
+
+            break;
+        case "opinion":
+            if (regExFormObject.opinion.test(e.target.value) == true) {
+                document.querySelector('#opinion').classList.remove('formInvalid');
+                document.querySelector('#opinion').classList.add('formAcepted');
+                document.querySelector('.formOpinion .formTextError').classList.remove('formTextErrorActive')
+                // verify['telf'] = true;
+
+            } else {
+                document.querySelector('#opinion').classList.add('formInvalid');
+                document.querySelector('#opinion').classList.remove('formAcepted');
+                document.querySelector('.formOpinion .formTextError').classList.add('formTextErrorActive')
+                // verify['telf'] = false;
+
+            }
+            break;
+    }
+
+}
+inputsForm.forEach((input) => {
+    input.addEventListener('keyup', verifyEverySingleCharacter);
+});
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+// const terms = document.getElementById('aceptpriv');
+    // if ((verify.usuario && verify.email && verify.telf && verify.opinion)==true) {
+    //     inputsForm.reset();
+    //     document.getElementById('formDataAcepted').classList.add('formDataAceptedActive')
+    // }
+    
+// });
+let submit = document.getElementById('submit')
+submit.onclick = function(){
+    
+    let inputName = document.querySelector('#username').value;
+    let inputEmail = document.querySelector('#email').value;
+    let inputOpinion = document.querySelector('#opinion').value;
+    let inputTlf = document.querySelector('#tlf').value;
+    localStorage.setItem("name", JSON.stringify(inputName));
+    localStorage.setItem("Email", JSON.stringify(inputEmail));
+    localStorage.setItem("Opinion", JSON.stringify(inputOpinion));
+    localStorage.setItem("telf", JSON.stringify(inputTlf));    
+}
 // FORM END
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,23 +1,58 @@
 
-
+//#region TESTS
 // console.log(prizeItemCartShop)
 // function addItemsToShopCart() {
-    let prizeItemCartShop = localStorage.getItem("prize");
-    let nameItemCartShop = localStorage.getItem("name");
-    let imgItemCartShop = localStorage.getItem("img");
-    
-    let imgSpace = document.getElementById('imgProductsShopCart')
-    let nameSpace = document.getElementById('nameProductsShopCart')
-    let prizeSpace = document.getElementById('prizeProductsShopCart')
-    // let divShoppingCart = document.createElement('div');
-    imgSpace.lastChild.src = imgItemCartShop;
-    nameSpace.innerText = nameItemCartShop;
-    prizeSpace.innerText = prizeItemCartShop;
+// ##########################
+// let prizeItemCartShop = localStorage.getItem("prize");
+// let nameItemCartShop = localStorage.getItem("name");
+// let imgItemCartShop = localStorage.getItem("img");
+// #####################
+// ===DIVS DEL HTML===
+//===ARRAYS VACIOS===
+//===ARRAYS VACIOS DENTRO DE DIVS DEL HTML===
+// let imgSpaceEmptyArray = imgSpace.appendChild(imgArrayItemCartShop);
+// let nameSpaceEmptyArray = nameSpace.appendChild(nameArrayItemCartShop);
+// let prizeSpaceEmptyArray = prizeSpace.appendChild(prizeArrayItemCartShop);
+//===JSON.parse===
+// let imgItemCartShop = (localStorage.getItem("img"));
+// console.log(imgItemCartShop)
+// let nameItemCartShop = (localStorage.getItem("name"));
+// let prizeItemCartShop = (localStorage.getItem("prize"));
+// console.log(prizeItemCartShop)
+//==Array.push
+// imgArrayItemCartShop.push(imgItemCartShop);
+// nameArrayItemCartShop.push(nameItemCartShop);
+// prizeArrayItemCartShop.push(prizeItemCartShop);
+//===
+// localStorage.setItem("imgArrayItemCartShop", JSON.stringify(imgItemCartShop));
+// localStorage.setItem("nameArrayItemCartShop", JSON.stringify(nameItemCartShop));
+// localStorage.setItem("prizeArrayItemCartShop", JSON.stringify(prizeItemCartShop));
 
+// let divShoppingCart = document.createElement('div');
+// ########################PRINTA EN LA CESTA
+// imgSpace.lastChild.src = imgItemCartShop;
+// nameSpace.innerText = nameItemCartShop;
+// prizeSpace.innerText = prizeItemCartShop;
+// #############################
 // }
+//#endregion
+// let arrayVacio = []
+// let elarrayconalgo = arrayVacio.push(elputonombre)
+// nameSpace.appendChild(elarrayconalgo)
 
 
-// function addItemToShoppingCart(tittleShopItems, imgShopItems, prizeShopItems){
-//     let shoppingCartRow = document.createElement('div');
-//     let shoppingDoc
-// }
+// let elputonombre = localStorage.getItem("name")
+let nameSpace = document.getElementById('nameProductsShopCart')
+let imgSpace = document.getElementById('imgProductsShopCart')
+let prizeSpace = document.getElementById('prizeProductsShopCart')
+// let nameArrayItemCartShop = nameSpace.append([(localStorage.getItem("name"))]);
+// let prizeArrayItemCartShop = prizeSpace.append([(localStorage.getItem("prize"))]);
+// let imgArrayItemCartShop = imgSpace.lastChild.src = [(localStorage.getItem("img"))];
+
+let dataCartShopInShopPage = localStorage.getItem('dataCartShop');
+let dataCartObj = JSON.parse(dataCartShopInShopPage);
+let objectData = Object.values(dataCartObj)
+
+imgSpace.lastChild.src = objectData[0]
+nameSpace.textContent = objectData[1]
+prizeSpace.textContent = objectData[2]
